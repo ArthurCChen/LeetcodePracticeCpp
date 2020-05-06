7d578f054160da60928f334f13748e07aaf32c41
chen-yn17 <cyn10221017@163.com>
2020-05-06 23:56:59 +0800

    20050506_98383_最低票价 some changes 2

--- 200506_983_最低票价.cpp
+++ 200506_983_最低票价.cpp
@@ -41,115 +41,67 @@
 // costs.length == 3
 // 1 <= costs[i] <= 1000
 
-// https://leetcode-cn.com/problems/validate-binary-search-tree/
+// https://leetcode-cn.com/problems/minimum-cost-for-tickets/submissions/
 
 
 
 //My Codes
 // 执行结果：通过
-// 执行用时 :16 ms, 在所有 C++ 提交中击败了75.10%的用户
-// 内存消耗 :18.2 MB, 在所有 C++ 提交中击败了100.00%的用户
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
-    bool isValidBST(TreeNode* root) 
-    {
-        TreeNode *last = NULL;
-        return isValidBST(root, last);
-    }
-    
-    bool isValidBST(TreeNode* root, TreeNode* &last)
-    { 
-        if(!root) return true;
-        if(!isValidBST(root->left, last) || (last && last->val >= root->val)) 
-            return false;
-        last = root;                          
-        return isValidBST(root->right, last); 
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int dayOfPass[] = {1, 7, 30};
+        int totalCost[366] = {0};
+        int i = 1;
+        int start;
+        for(int date: days){
+            while(i < date){
+                totalCost[i] = totalCost[i - 1];
+                i++;
+            }
+            
+            totalCost[i] = INT_MAX; 
+            for(int ticket = 0; ticket < 3; ticket++){
+                start = i - dayOfPass[ticket];
+                if(start < 0)  start = 0;
+                totalCost[i] = min(totalCost[i], totalCost[start] + costs[ticket]);
+            }
+            i++;
+        }
+        return totalCost[days.back()];
     }
 };
 
 
+
 //nice codes:
-// 方法一: 递归
-// 思路和算法
+// 动态规划问题最重要是3步：
 
-// 要解决这道题首先我们要了解二叉搜索树有什么性质可以给我们利用，由题目给出的信息我们可以知道：如果该二叉树的左子树不为空，则左子树上所有节点的值均小于它的根节点的值； 若它的右子树不空，则右子树上所有节点的值均大于它的根节点的值；它的左右子树也为二叉搜索树。
+// 明确创建怎样的dp数组
+// 初始化dp数组
+// 明确动态转移方程
+// 对于本题不难想到应该用一个数组存储到当前某一天需要花费的最少费用，这里为了下标和天数对应，dp数组的长度选择 days 中最后一个天数多加 1 个长度，因为开始没有费用，所以初始化为 0 ，之后开始对 dp 数组进行更新，那么每到达一个位置首先考虑当前天数是否在days 中，如果不在那花费的费用肯定和它前一天花费的最少费用相同(这里用一个 idx 指标指示应该处理哪一个天数，这样就不必用 if i in days 这样的语句判断天数是否需要处理了，可以让程序快一些)，如果在的话，我们就要从三种购买方式中选择一种花费费用最少的，即你想到达第 i 天，你需要从 i 的前1或7或30天的后一位置花费对应cost[0]、cost[1]、cost[2]的钱才能到第 i 天。
+// 具体细节见代码。
 
-// 这启示我们设计一个递归函数 helper(root, lower, upper) 来递归判断，函数表示考虑以 root 为根的子树，判断子树中所有节点的值是否都在 (l,r)(l,r) 的范围内（注意是开区间）。如果 root 节点的值 val 不在 (l,r)(l,r) 的范围内说明不满足条件直接返回，否则我们要继续递归调用检查它的左右子树是否满足，如果都满足才说明这是一棵二叉搜索树。
+// 视频讲解：
 
-// 那么根据二叉搜索树的性质，在递归调用左子树时，我们需要把上界 upper 改为 root.val，即调用 helper(root.left, lower, root.val)，因为左子树里所有节点的值均小于它的根节点的值。同理递归调用右子树时，我们需要把下界 lower 改为 root.val，即调用 helper(root.right, root.val, upper)。
+// 🐼代码部分
+// class Solution:
+//     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+//         dp = [0 for _ in range(days[-1] + 1)]  # dp数组，每个元素代表到当前天数最少钱数，为下标方便对应，多加一个 0 位置
+//         days_idx = 0  # 设定一个days指标，标记应该处理 days 数组中哪一个元素
+//         for i in range(1, len(dp)):
+//             if i != days[days_idx]:  # 若当前天数不是待处理天数，则其花费费用和前一天相同
+//                 dp[i] = dp[i - 1]
+//             else:
+//                 # 若 i 走到了待处理天数，则从三种方式中选一个最小的
+//                 dp[i] = min(dp[max(0, i - 1)] + costs[0],
+//                             dp[max(0, i - 7)] + costs[1],
+//                             dp[max(0, i - 30)] + costs[2])
+//                 days_idx += 1
+//         return dp[-1]  # 返回最后一天对应的费用即可
 
-// 函数递归调用的入口为 helper(root, -inf, +inf)， inf 表示一个无穷大的值。
-
-// 下图展示了算法如何应用在示例 2 上：
-
-
-// 1 / 4
-
-// C++
-// class Solution {
-// public:
-//     bool helper(TreeNode* root, long long lower, long long upper) {
-//         if (root == nullptr) return true;
-//         if (root -> val <= lower || root -> val >= upper) return false;
-//         return helper(root -> left, lower, root -> val) && helper(root -> right, root -> val, upper);
-//     }
-//     bool isValidBST(TreeNode* root) {
-//         return helper(root, LONG_MIN, LONG_MAX);
-//     }
-// };
-// 复杂度分析
-
-// 时间复杂度 : O(n)O(n)，其中 nn 为二叉树的节点个数。在递归调用的时候二叉树的每个节点最多被访问一次，因此时间复杂度为 O(n)O(n)。
-
-// 空间复杂度 : O(n)O(n)，其中 nn 为二叉树的节点个数。递归函数在递归过程中需要为每一层递归函数分配栈空间，所以这里需要额外的空间且该空间取决于递归的深度，即二叉树的高度。最坏情况下二叉树为一条链，树的高度为 nn ，递归最深达到 nn 层，故最坏情况下空间复杂度为 O(n)O(n) 。
-
-// 方法二：中序遍历
-// 思路和算法
-
-// 基于方法一中提及的性质，我们可以进一步知道二叉搜索树「中序遍历」得到的值构成的序列一定是升序的，这启示我们在中序遍历的时候实时检查当前节点的值是否大于前一个中序遍历到的节点的值即可。如果均大于说明这个序列是升序的，整棵树是二叉搜索树，否则不是，下面的代码我们使用栈来模拟中序遍历的过程。
-
-// 可能由读者不知道中序遍历是什么，我们这里简单提及一下，中序遍历是二叉树的一种遍历方式，它先遍历左子树，再遍历根节点，最后遍历右子树。而我们二叉搜索树保证了左子树的节点的值均小于根节点的值，根节点的值均小于右子树的值，因此中序遍历以后得到的序列一定是升序序列。
-
-
-
-// C++
-// class Solution {
-// public:
-//     bool isValidBST(TreeNode* root) {
-//         stack<TreeNode*> stack;
-//         long long inorder = (long long)INT_MIN - 1;
-
-//         while (!stack.empty() || root != nullptr) {
-//             while (root != nullptr) {
-//                 stack.push(root);
-//                 root = root -> left;
-//             }
-//             root = stack.top();
-//             stack.pop();
-//             // 如果中序遍历得到的节点的值小于等于前一个 inorder，说明不是二叉搜索树
-//             if (root -> val <= inorder) return false;
-//             inorder = root -> val;
-//             root = root -> right;
-//         }
-//         return true;
-//     }
-// };
-// 复杂度分析
-
-// 时间复杂度 : O(n)O(n)，其中 nn 为二叉树的节点个数。二叉树的每个节点最多被访问一次，因此时间复杂度为 O(n)O(n)。
-
-// 空间复杂度 : O(n)O(n)，其中 nn 为二叉树的节点个数。栈最多存储 nn 个节点，因此需要额外的 O(n)O(n) 的空间。
-
-// 作者：LeetCode-Solution
-// 链接：https://leetcode-cn.com/problems/validate-binary-search-tree/solution/yan-zheng-er-cha-sou-suo-shu-by-leetcode-solution/
+// 作者：LotusPanda
+// 链接：https://leetcode-cn.com/problems/minimum-cost-for-tickets/solution/xiong-mao-shua-ti-python3-dong-tai-gui-hua-yi-do-2/
 // 来源：力扣（LeetCode）
 // 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
